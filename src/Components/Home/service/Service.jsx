60fc976f3e68035b0f4ef50e2e817040ad743005
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-10-06 11:59:10 +0600

    adding service section

---
+++ src/Components/Home/service/Service.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ServiceCard from './serviceCard/ServiceCard';
+
+const Service = () => {
+    return (
+        <div className="container">
+            <div className="title-contaner">
+                <h2>Our <span color="#FF6600">Popular</span> Services</h2>
+                <p>Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
+            </div>
+            <div className="service-card-container d-md-flex flex-row ">
+                <ServiceCard></ServiceCard>
+                <ServiceCard></ServiceCard>
+                <ServiceCard></ServiceCard>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/Components/Home/Banner/Banner";
+import Service from "@/Components/Home/service/Service";
 
 export default function Home() {
   return (
     <div className="">
      <Banner/>
+     <Service/>
     </div>
   );
 }

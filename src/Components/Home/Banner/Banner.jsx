fb174e00db785ebc0870e7128ba193e13cc014e2
webcodesky1 <webcodesky1@gmail.com>
2024-10-07 19:36:29 +0600

    create new branch

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import {  FaCircleArrowUp } from "react-icons/fa6";
+import { FaCircleArrowUp } from "react-icons/fa6";
 
 const Banner = () => {
   return (
     <div className="relative bg-[#E7E7E7]">
       <div className="container mx-auto min-h-screen">
         <p className="text-[18px] font-medium pt-[72px]">
-          We Build Your Dream Website
+          We Build Your Dream Website todo
         </p>
         <h1 className="font-bold text-[40px] md:text-[190px] tracking-[-4%]">
           Web <span className="text-[#FF6600]">Code</span> Sky
@@ -16,10 +16,12 @@ const Banner = () => {
           of Classical physics: Newtonian mechanics{" "}
         </p>
         <div className="flex justify-center">
-        <button className="mt-[30px] border-[1px] rounded-[50px] px-6 py-[21px] hover:bg-[#FF6600] flex gap-5">
-        <span className="text-[24px]">Get a Quote</span>
-        <span className="text-white text-[40px] rotate-45"><FaCircleArrowUp/></span>
-        </button>
+          <button className="mt-[30px] border-[1px] rounded-[50px] px-6 py-[21px] hover:bg-[#FF6600] flex gap-5">
+            <span className="text-[24px]">Get a Quote</span>
+            <span className="text-white text-[40px] rotate-45">
+              <FaCircleArrowUp />
+            </span>
+          </button>
         </div>
       </div>
     </div>

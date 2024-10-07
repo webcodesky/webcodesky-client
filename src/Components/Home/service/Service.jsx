b8a99c39a782ab705ad7556eb7fc66dd7ec9e3b9
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-10-07 23:22:58 +0600

    ---

--- src/Components/Home/service/Service.jsx
+++ src/Components/Home/service/Service.jsx
@@ -4,8 +4,8 @@ import ServiceCard from './serviceCard/ServiceCard';
 const Service = () => {
     return (
         <div className="container">
-            <div className="title-contaner">
-                <h2>Our <span color="#FF6600">Popular</span> Services</h2>
+            <div className="title-contaner items-center">
+                <h2>Our <span className="text-[#FF6600]">Popular</span> Services</h2>
                 <p>Problems trying to resolve the conflict between the two major realms of Classical physics: Newtonian mechanics </p>
             </div>
             <div className="service-card-container d-md-flex flex-row ">
